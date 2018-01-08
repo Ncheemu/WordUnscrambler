15a18d422f177ab40894e67b9c1b9ebda401d840
Viggo Larsen <viggo.larsen@alyrica.net>
2018-01-08 12:54:05 -0800

    Add project files.

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case ("F"):
+                        Console.Write("Enter scrambled words file name: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case ("M"):
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsManualEntryScenario();
+                        break;
+                    default:
+                        Console.Write("Option was not recognized.");
+                        break;
+                }
+
+                var continueWordUnscrambleDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue?");
+                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                        !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+            } while (continueWordUnscramble);
+        }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
